c389a7cd97d170489ad26505d256aea35e5a03bf
Saiful Islam | iT Dept. <saiful@mail.si.sinat>
2026-05-11 21:54:37 +0600

    2nd time edit associative array

--- associative.sh
+++ associative.sh
@@ -3,12 +3,12 @@ declare -A user_info
 user_info["rahim"]="Engineer, 30, Dhaka"
 user_info["karim"]="Doctor, 35, Chittagong"
 user_info["fatema"]="Teacher, 28, Sylhet"
-
+echo -e "now we show value"
 echo "Rahim's Information: ${user_info["rahim"]}"
 echo "Karim's Information: ${user_info["karim"]}"
 echo " "
-echo "All user information:"
-for key in "${!user_info[@]}"; do
+echo "All key show:"
+for key in "${user_info[@]}"; do
 	echo " - $key"
 done
 echo " "
