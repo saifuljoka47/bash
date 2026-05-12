b611b7b82ba6d88c57e36449494e2dbafab361fb
Saiful Islam | iT Dept. <saiful@mail.si.sinat>
2026-05-12 22:18:05 +0600

    check.sh files edit first time

--- check.sh
+++ check.sh
@@ -9,6 +9,49 @@ check() {
 	fi
 }
 
-echo -e "Type a service name"
-read service
-check $service
+echo -e "We are checking services"
+menu() {
+	echo -e "Here we show menu options"
+	echo -e "1.Predefined services like httpd sshd"
+	echo -e "2.single service like httpd"
+	echo -e "3.Multi service"
+	echo -e "4.Exit"
+	echo -e "Between 1 to 4 press any number"
+}
+predefined() {
+	service=("httpd" "sshd" "firewalld")
+	for serv in "${service[@]}"; do
+		check $serv
+	done
+}
+single() {
+	echo -e "Just type one service name and press enter"
+	read service
+	check $service
+}
+multi() {
+	echo -e "Now you can type many services"
+	read -a service
+	for serv in "${service[@]}"; do
+		check $serv
+	done
+}
+while true; do
+	menu
+	read choice
+	if [ $choice -eq 1 ]; then
+		echo -e "you choose option 1"
+		predefined
+	elif [ $choice -eq 2 ]; then
+		echo -e "you choose option 2"
+		single
+	elif [ $choice -eq 3 ]; then
+		echo -e "you choose option 3"
+		multi
+	elif [ $choice -eq 4 ]; then
+		echo -e "you choose option 4\n Now time to say goodby"
+		break
+	else
+		echo -e "You enter wrong number \n press between 1-4"
+	fi
+done
