8c1fe082f081f85806d405521448d531d82a93ab
Saiful Islam | iT Dept. <saiful@mail.si.sinat>
2026-05-13 11:17:56 +0600

    menu.sh update

--- menu.sh
+++ menu.sh
@@ -1,26 +1,52 @@
 #!/bin/bash
+service() {
+	if systemctl status $1 &>/dev/null; then
+		if systemctl is-active --quiet $1 2>/dev/null; then
+			echo "the service $1 is running"
+		else
+			echo "the service $1 not active"
+		fi
+	fi
+}
+predefined() {
+	services=("httpd" "sshd" "firewalld")
+	for serv in "${services[@]}"; do
+		service $serv
+	done
+}
+single() {
+	echo -e "now type only one service name like sshd"
+	read services
+	service $services
+}
+multi() {
+	echo -e "Now you type multi service name like httpd firewalld"
+	read -a services
+	for serv in "${services[@]}"; do
+		service $serv
+	done
+}
+menu() {
 echo -e "Here we show menu Now"
 echo "1.predefined menu"
 echo "2.Single menu"
 echo "3.Multiple menu"
 echo "4.Exit"
-read number
+}
 while true; do
+	menu
+	read number
 	if [ $number -eq 1 ]; then
-		echo -e "You typed number 1\n Thanks"
-		break
+		predefined
 	elif [ $number -eq 2 ]; then
-		echo -e "you typed number 2\n Thanks"
-		break
+		single
 	elif [ $number -eq 3 ]; then
-		echo -e "you typed number 3\n Thanks"
-		break
+		multi
 	elif [ $number -eq 4 ]; then
 		echo -e "Exit"
 		break
 	else
 		echo "wrong number.press between 1-4.Thanks"
-		break
 	fi
 done
 		
