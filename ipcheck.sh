ff5b6cad1db23ac16319ce14dccafce32bb5bce6
Saiful Islam | iT Dept. <saiful@mail.si.sinat>
2026-05-13 11:30:24 +0600

    update on 13-05-26

--- ipcheck.sh
+++ ipcheck.sh
@@ -4,12 +4,12 @@
 echo "I checking your ips"
 ips=("192.168.61." "192.168.62." "192.168.63." "192.168.64." "192.168.65." "192.168.66." "192.168.67." "192.168.68." "192.168.69." "192.168.70." "192.168.71." "192.168.72." "192.168.73." "192.168.74." "192.168.75." "192.168.76." "192.168.80.")
 for ip in "${ips[@]}"; do
-	for i in {1..3}; do
+	for i in {1..10}; do
 	if	ping -c 1 -W 1 "${ip}${i}" &>/dev/null; then
-		echo "Online"
+		echo "Online ${ip}${i}"
 		echo "${ip}${i}" >> /bash/online.txt
 	else
-		echo "Offline"
+		echo "Offline ${ip}${i}"
 		echo "${ip}${i}" >> /bash/offline.txt
 	fi
 done
