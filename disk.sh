7d9659a671b837255cc1f5fa46c5bdd0c212fddb
Saiful Islam | iT Dept. <saiful@mail.si.sinat>
2026-05-13 12:07:32 +0600

    update on 13-05-26 time-12-08PM

--- disk.sh
+++ disk.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo -e "Here we check disk status"
-df -h -x iso9660 | awk 'NR>1' | awk '{print $5 " " $6}' | while read disk; do
+df -h -x iso9660 | awk 'NR>1' | awk '{print $5 " " $6 }' | while read disk; do
 usage=$( echo $disk | awk '{print $1}' | sed 's/%//')
-disk_name=$( echo $dik | awk '{print $2}')
-echo $usage
+disk_name=$( echo $disk | awk '{print $2}')
+if [ $usage -ge 20 ]; then
+	echo -e "The disk name $disk_name is $usage%\n Please take necessary action"
+fi
 done
